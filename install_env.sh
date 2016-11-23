8371848c8816e1c887cb40664a11e9d01506a787
Jordan Lin <jordan.lin@medicustek.com>
2016-11-23 18:07:25 +0800

    Add install script

---
+++ install_env.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Nonsensejr/vimrc ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+git clone https://github.com/Nonsensejr/oh-my-zsh zsh_install
+sh ~/zsh_install/tools/install.sh
