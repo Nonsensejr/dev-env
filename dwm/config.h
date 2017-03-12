2cdbb8fd4bae3574af1ed03d5b2a785fbc04621f
Jord Lin <jordan.lin@medicustek.com>
2017-03-12 19:32:26 +0800

    change number to chinese for radness

--- dwm/config.h
+++ dwm/config.h
@@ -17,7 +17,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "壹","貳","叁","肆","伍","陸","柒","捌","玖" };
 
 static const Rule rules[] = {
 	/* xprop(1):
